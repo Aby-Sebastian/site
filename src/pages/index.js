3db5d0c2088d4ffe7ac14295f163f8675bcb6ee5
Aby-Sebastian <abysebastian98@gmail.com>
2020-02-03 18:21:39 +0530

    commented gatsby image

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import React from "react"
 import { graphql, useStaticQuery } from "gatsby"
 import Layout from "../components/Layout"
-import Img from "gatsby-image"
+// import Img from "gatsby-image"
 
 const IndexPage = () => {
   const data = useStaticQuery(graphql`
