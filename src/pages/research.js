c7425c62f63f225d44b5077c9169becdcc2401b8
Aby-Sebastian <abysebastian98@gmail.com>
2020-02-03 18:21:57 +0530

    created 2 new pages

---
+++ src/pages/research.js
@@ -0,0 +1,8 @@
+import React from "react"
+import Layout from "../components/Layout"
+
+function research() {
+  return <Layout>Research</Layout>
+}
+
+export default research
---
+++ src/pages/service.js
@@ -0,0 +1,8 @@
+import React from "react"
+import Layout from "../components/Layout"
+
+const service = () => {
+  return <Layout>Service</Layout>
+}
+
+export default service
