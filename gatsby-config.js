c773ab95e3db3e160425765c1a99172d7e8f9abe
Aby-Sebastian <abysebastian98@gmail.com>
2020-02-04 09:31:11 +0530

    changed  back to pages and given blogs a path

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,6 +15,13 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
+        name: `pages`,
+        path: `${__dirname}/src/pages/`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
         name: `blogs`,
         path: `${__dirname}/src/blogs/`,
       },
