a3e5bbda78f054d0da8b71a0e6bfabcc05353a21
Aby-Sebastian <abysebastian98@gmail.com>
2020-02-04 15:50:43 +0530

    displays all blogs with short description

--- src/pages/research.js
+++ src/pages/research.js
@@ -1,12 +1,41 @@
 import React from "react"
 import Layout from "../components/Layout"
+import { Link } from "gatsby"
 
-function research() {
+function research({ data }) {
   return (
     <Layout>
-      <div className="container">Research</div>
+      <div className="container">
+        Research
+        <br />
+        {data.allMarkdownRemark.edges.map(({ node }) => (
+          <>
+            <h1>
+              <Link to={node.frontmatter.title}>{node.frontmatter.title}</Link>
+            </h1>
+            <div>{node.frontmatter.description}</div>
+          </>
+        ))}
+      </div>
     </Layout>
   )
 }
 
 export default research
+
+export const query = graphql`
+  {
+    allMarkdownRemark {
+      edges {
+        node {
+          frontmatter {
+            title
+            description
+          }
+          excerpt
+          fileAbsolutePath
+        }
+      }
+    }
+  }
+`
