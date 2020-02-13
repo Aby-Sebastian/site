6dac6b13bae5938f2aedd085df4b2d0d9c0fc6ca
Aby-Sebastian <abysebastian98@gmail.com>
2020-02-13 19:46:38 +0530

    the queried data now passes to header file

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -3,11 +3,25 @@ import Header from "./header"
 import FooterPage from "./footer"
 import "./main.css"
 import "./bootstrap.min.css"
+import { useStaticQuery, graphql } from "gatsby"
 
 const Layout = ({ children }) => {
+  const data = useStaticQuery(graphql`
+    query SiteTitleQuery {
+      site {
+        siteMetadata {
+          title
+          menuLinks {
+            name
+            link
+          }
+        }
+      }
+    }
+  `)
   return (
     <div className="bg-color">
-      <Header />
+      <Header menuLinks={data.site.siteMetadata.menuLinks} />
       {children}
       <FooterPage />
     </div>
