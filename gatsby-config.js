4dea820089ed3b05619462ad1eac248df32c0617
Aby-Sebastian <abysebastian98@gmail.com>
2020-02-17 18:12:29 +0530

    icons are added to create social link section in footer

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,7 +25,7 @@ module.exports = {
       },
       {
         name: "Resource",
-        link: "/",
+        link: "/resource",
       },
       {
         // name: "About Us",
---
+++ src/pages/resource.js
@@ -0,0 +1,39 @@
+import React from "react"
+// import { Link } from "gatsby"
+import facebook from "../img/social/facebook.svg"
+import instagram from "../img/social/instagram.svg"
+import twitter from "../img/social/twitter.svg"
+import vimeo from "../img/social/vimeo.svg"
+
+const Resource = () => (
+  <div className="container">
+    <div className="column is-4 social">
+      <a title="facebook" href="https://facebook.com">
+        <img
+          src={facebook}
+          alt="Facebook"
+          style={{ width: "1em", height: "1em" }}
+        />
+      </a>
+      <a title="twitter" href="https://twitter.com">
+        <img
+          className="fas fa-lg"
+          src={twitter}
+          alt="Twitter"
+          style={{ width: "1em", height: "1em" }}
+        />
+      </a>
+      <a title="instagram" href="https://instagram.com">
+        <img
+          src={instagram}
+          alt="Instagram"
+          style={{ width: "1em", height: "1em" }}
+        />
+      </a>
+      <a title="vimeo" href="https://vimeo.com">
+        <img src={vimeo} alt="Vimeo" style={{ width: "1em", height: "1em" }} />
+      </a>
+    </div>
+  </div>
+)
+export default Resource
