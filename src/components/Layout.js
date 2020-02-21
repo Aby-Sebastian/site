d1f7258c70bad72faf8f1cdca05615fa5d7c0f65
Aby-Sebastian <abysebastian98@gmail.com>
2020-02-21 19:02:16 +0530

    sadsa

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -29,33 +29,7 @@ const Layout = ({ children }) => {
         <html lang="en" />
         <title>{title}</title>
         <meta name="description" content={description} />
-        <meta property="og:type" content="article" />
-        <meta property="og:title" content={title} />
-        <meta
-          property="og:url"
-          content="https://infallible-cori-68e745.netlify.com/"
-        />
-
-        <meta
-          property="og:image"
-          content={`${withPrefix("/")}img/Opnlogo.png`}
-        />
-        <meta
-          data-rh="true"
-          name="twitter:image:src"
-          content="https://miro.medium.com/max/1200/1*qpUWqfJQSuTO-Bc4DuHBcw.jpeg"
-        />
-        <meta
-          data-rh="true"
-          name="twitter:card"
-          content="summary_large_image"
-        />
-        <meta
-          data-rh="true"
-          property="article:author"
-          content="https://medium.com/@fasterpancakes"
-        />
-        <meta data-rh="true" name="twitter:creator" content="@fasterpancakes" />
+        
       </Helmet>
       <div className="bg-color">
         <Header menuLinks={data.site.siteMetadata.menuLinks} />
