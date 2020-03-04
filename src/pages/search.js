6515399b6184af968289af5e9e6533022a5b20b1
Aby-Sebastian <abysebastian98@gmail.com>
2020-03-04 14:16:03 +0530

    autofocus added

--- src/pages/search.js
+++ src/pages/search.js
@@ -54,6 +54,7 @@ const BlogIndex = props => {
             aria-label="Search"
             placeholder="Type to filter posts..."
             onChange={handleInputChange}
+            autoFocus
           />
         </div>
 
