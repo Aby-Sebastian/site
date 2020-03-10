e97d9a7ebf572975f423b14de7e8f2102ec0e865
Aby-Sebastian <abysebastian98@gmail.com>
2020-03-10 19:59:39 +0530

    installed gatsby-remark-images and gatsby-remark-relative-images

--- gatsby-config.js
+++ gatsby-config.js
@@ -69,7 +69,36 @@ module.exports = {
         path: `${__dirname}/src/img/logos/`,
       },
     },
-    `gatsby-transformer-remark`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/static/img`,
+        name: "img",
+      },
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          // gatsby-remark-relative-images must
+          // go before gatsby-remark-images
+          {
+            resolve: `gatsby-remark-relative-images`,
+            name: "images",
+          },
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 500,
+              loading: "lazy",
+              // It's important to specify the maxWidth (in pixels) of
+              // the content container as this plugin uses this as the
+              // base for generating different widths of each image.
+            },
+          },
+        ],
+      },
+    },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     `gatsby-plugin-netlify-cms`,
